5ae6d96dca87402fe2bd60ae8d5f4c2c70b7045d
Walle692 <146271763+Walle692@users.noreply.github.com>
2024-12-09 08:56:02 +0100

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -2,10 +2,14 @@
 # Lab 3, D0012E
 # Albin Täljsten, Linus Fridlund, Walter Wimmercranz
 #
+import BinarySearchTree
+import Node
+
 
 def main():
-
-
+    tree = BinarySearchTree
+    node = Node(2, 3)
+    
 
 
 main()
