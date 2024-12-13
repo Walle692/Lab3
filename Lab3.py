ebd05a04d82dad8c2d134c6eafbf86cc89b85b7e
Walle692 <walwim-2@student.ltu.se>
2024-12-13 16:11:38 +0100

    bla

--- Lab3.py
+++ Lab3.py
@@ -20,21 +20,22 @@ def nodegenerator(tree, nodes):
 def superrandomnodegenerator(tree, nodes):
     nodekey_list = []
     for i in range(0,nodes):
-        new_key = randint(0,100000000)
+        new_key = randint(0,100000)
         if new_key not in nodekey_list:
             nodekey_list.append(new_key)
     for i in range(0,len(nodekey_list)):
         newNode = Node( randint(0,4000),nodekey_list[i])
         tree.insert(newNode)
-    return tree, nodekey_list
+    return tree, len(nodekey_list)
 
 
 def bst_tester(tree, nodes):
     start_time = time.time()
-    tree, nodes = nodegenerator(tree, nodes)
+    tree, nodes = superrandomnodegenerator(tree, nodes)
     insert_time = time.time() - start_time
+    print(nodes)
     start_time = time.time()
-    tree.search(tree.getroot(), nodes)
+    tree.search(tree.getroot(), 100001)
     search_time = time.time() - start_time
     return search_time, insert_time
 
@@ -53,26 +54,22 @@ def main():
         d5 = D(0.95)
         search_time_bst1, insert_time_bst1 = bst_tester(bst1, integers)
         print("BST, nodes: ", integers, " search time: ", search_time_bst1," insertion time: ", insert_time_bst1)
+        del bst1
         search_time_d1, insert_time_d1 = bst_tester(d1, integers)
         print("D(0.55), nodes: ", integers, " search time: ", search_time_d1," insertion time: ", insert_time_d1)
+        del d1
         search_time_d2, insert_time_d2 = bst_tester(d2, integers)
         print("D(0.65), nodes: ", integers, " search time: ", search_time_d2," insertion time: ", insert_time_d2)
+        del d2
         search_time_d3, insert_time_d3 = bst_tester(d3, integers)
         print("D(0.75), nodes: ", integers, " search time: ", search_time_d3," insertion time: ", insert_time_d3)
+        del d3
         search_time_d4, insert_time_d4 = bst_tester(d4, integers)
         print("D(0.85), nodes: ", integers, " search time: ", search_time_d4," insertion time: ", insert_time_d4)
+        del d4
         search_time_d5, insert_time_d5 = bst_tester(d5, integers)
         print("D(0.95), nodes: ", integers, " search time: ", search_time_d5," insertion time: ", insert_time_d5)
-        del bst1
-        del d1
-        del d2
-        del d3
-        del d4
         del d5
-
-
-
-
 
 
 
